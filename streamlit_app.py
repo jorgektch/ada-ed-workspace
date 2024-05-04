527618879d173ba60cb3a3b7fa275dd642202dcd
Jorge Jesús <jorgejesus.quispe@unmsm.edu.pe>
2024-05-03 22:26:28 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,20 @@
+### 1. Descripción del proyecto
+El proyecto consiste en análisis exploratorio de un dataset que registra información
+de los casos positivos detectados en Perú durante el mes de mayo del año 2022.
+
+Fuente de datos: [https://ejemplo.com/](https://ejemplo.com/)
+
+### 2. Origen de los datos
+El conjunto de dato se obtuvo del portal de datos abiertos del MINSA.
+| Variable        |	Descripción                                                                                         |
+| --------------- | --------------------------------------------------------------------------------------------------- |
+| FECHA_CORTE     |	Fecha de corte de información                                                                       |
+| UUID            |	ID de la persona como caso positivo de covid-19 confirmada con cualquier tipo de prueba.            |
+| UBIGEO          |	Código de Ubicación Geografica que denotan "DDppdd" (Departamento, provincia,distrito), fuente INEI |
+| DEPARTAMENTO    |	Departamento donde reside la persona confirmada como caso positivo de covid-19                      |
+| PROVINCIA       |	Provincia donde reside la persona confirmada como caso positivo de covid-19                         |
+| DISTRITO        |	Distrito donde reside la persona confirmada como caso positivo de covid-19                          |
+| METODODX        |	Metodo de laboratorio a la que es sometida una prueba de covid-19                                   |
+| EDAD            |	Edad de la persona confirmada como caso positivo de covid-19                                        |
+| SEXO            |	Sexo de la persona confirmada como caso positivo de covid-19                                        |
+| FECHA_RESULTADO |	Fecha del resultado de la prueba de covid-19                                                        |
