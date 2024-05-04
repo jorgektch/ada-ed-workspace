b34230c3efa09487d09f98d5e7db4bc35a119fa8
Jorge Jesús <jorgektch@gmail.com>
2024-05-04 10:32:20 -0500

    Update and rename semana_01_latex.py to semana_01_analisis.py

--- semana_01_analisis.py
+++ semana_01_analisis.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-def ejecutar_semana_01_latex():
+def ejecutar_semana_01_analisis():
   st.latex(r'''
       a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
       \sum_{k=0}^{n-1} ar^k =
