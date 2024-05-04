bc8c9c6fdd16cf07363df1e986fb421e5419174b
Jorge Jesús <jorgektch@gmail.com>
2024-05-04 10:05:09 -0500

    Update semana_01.py

--- semana_01.py
+++ semana_01.py
@@ -1,9 +1,6 @@
 import streamlit as st
+from semana_01_latex import *
 
 def ejecutar_semana_01():
   st.write(f"Esta la semana 01")
-  st.latex(r'''
-    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-    \sum_{k=0}^{n-1} ar^k =
-    a \left(\frac{1-r^{n}}{1-r}\right)
-    ''')
+  st.ejecutar_semana_01_latex()
