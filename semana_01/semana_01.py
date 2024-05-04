949af6bb0af8638607d1ce53ebece728c3ef2652
Jorge Jesús <jorgektch@gmail.com>
2024-05-04 12:27:09 -0500

    Update semana_01.py

--- semana_01/semana_01.py
+++ semana_01/semana_01.py
@@ -1,12 +1,12 @@
 import streamlit as st
-from enunciado import *
+from .enunciado import *
 from implementacion import *
 from analisis import *
 from graficas import *
 
 def ejecutar():
   st.header(f"Semana 01")
-  enunciado.ejecutar_enunciado()
+  ejecutar_enunciado()
   ejecutar_implementacion()
   ejecutar_analisis()
   ejecutar_graficas()
