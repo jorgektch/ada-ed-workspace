650d2967e02e73a39cfadcaf6037488aab597372
Jorge Jesús <jorgektch@gmail.com>
2024-05-04 10:33:01 -0500

    Update semana_01_analisis.py

--- semana_01_analisis.py
+++ semana_01_analisis.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 def ejecutar_semana_01_analisis():
+  st.subheader("Análisis")
   st.latex(r'''
       a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
       \sum_{k=0}^{n-1} ar^k =
