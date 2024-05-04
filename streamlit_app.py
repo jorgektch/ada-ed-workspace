34ad7d5c48ee9f1ae7969ff86e2ccaca24dfc7ee
Jorge Jesús <jorgektch@gmail.com>
2024-05-04 12:48:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ st.title('Curso: Análisis y diseño de algoritmos')
 if "Inicio" in add_selectbox:
   ejecutar_inicio()
 if "Semana 01" in add_selectbox:
-  ejecutar()
+  ejecutar_semana_01()
 elif "Semana 02" in add_selectbox:
   ejecutar_semana_02()
 elif "Semana 03" in add_selectbox:
