313b58b04aeb281c9073b858a323e2a5830c1d06
Jorge Jesús <jorgektch@gmail.com>
2024-05-04 10:31:55 -0500

    Update semana_01.py

--- semana_01.py
+++ semana_01.py
@@ -1,6 +1,12 @@
 import streamlit as st
-from semana_01_latex import *
+from semana_01_enunciado import *
+from semana_01_implementacion import *
+from semana_01_analisis import *
+from semana_01_graficas import *
 
 def ejecutar_semana_01():
   st.header(f"Semana 01")
-  ejecutar_semana_01_latex()
+  ejecutar_semana_01_enunciado()
+  ejecutar_semana_01_implementacion()
+  ejecutar_semana_01_analisis()
+  ejecutar_semana_01_graficas()
