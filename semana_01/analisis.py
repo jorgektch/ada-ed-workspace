b6ddf20163c160c82ee6c29fd43a5523583dbaf5
Jorge Ktch <jorgektch@gmail.com>
2024-05-05 23:46:51 -0500

    Incluir texto

--- semana_01/analisis.py
+++ semana_01/analisis.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def ejecutar_analisis():
   st.subheader("Análisis")
-  st.write("Este es el enunciado")
+  st.write("Analizando el algoritmo")
   st.latex(r'''
       a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
       \sum_{k=0}^{n-1} ar^k =
