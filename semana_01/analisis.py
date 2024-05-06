01a9eaa7d98882346852fdb45c3afb5d5cde6e1d
Jorge Ktch <jorgektch@gmail.com>
2024-05-05 23:47:39 -0500

    Incluir texto

--- semana_01/analisis.py
+++ semana_01/analisis.py
@@ -3,7 +3,7 @@ import streamlit as st
 def ejecutar_analisis():
   st.subheader("Análisis")
   st.write("Analizando el algoritmo")
-  st.latex(r'''
+  st.latexxx(r'''
       a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
       \sum_{k=0}^{n-1} ar^k =
       a \left(\frac{1-r^{n}}{1-r}\right)
