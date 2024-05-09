de7cf9c3a03cef043dfc813736c8b83c3f3bc4e4
Alvaro Chavez <135331266+AlvaroChavezCc@users.noreply.github.com>
2024-05-08 23:51:43 -0500

    Update implementacion.py

--- semana_02/implementacion.py
+++ semana_02/implementacion.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-def ejecutar_implementacion():
-  st.subheader("Algoritmo")
+def ejecutar_implementacion10():
+  st.subheader("Algoritmo versión 1")
 
   code = '''public boolean hasTwoTrueValues( boolean [ ] arr ){
 	int count = 0;
@@ -13,3 +13,31 @@ def ejecutar_implementacion():
 	return count >= 2;
  }'''
   st.code(code, language='java')
+
+def ejecutar_implementacion11():
+  st.subheader("Algoritmo versión 2")
+
+  code = '''public boolean hasTwoTrueValues( boolean [ ] arr ){
+	for( int i = 0; i < arr.length; i++ )
+		for( int j = i + 1; j < arr.length; j++ )
+			if( arr[ i ] && arr[ j ] )
+				return true;
+
+	return false;
+ }'''
+  st.code(code, language='java')
+
+def ejecutar_implementacion12():
+  st.subheader("Algoritmo versión 3")
+
+  code = '''public boolean hasTwoTrueValues( boolean [ ] arr ){
+	for( int i = 0; i < arr.length; i++ )
+  		if( arr[ i ])
+		for( int j = i + 1; j < arr.length; j++ )
+			if( arr[ j ] )
+				return true;
+	return false;
+ }'''
+  st.code(code, language='java')
+
+
