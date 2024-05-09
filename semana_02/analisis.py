8ef7f63e60bae8d8a13f2a7502ec7fa2314a7e14
Alvaro Chavez <135331266+AlvaroChavezCc@users.noreply.github.com>
2024-05-08 23:10:54 -0500

    Update analisis.py

--- semana_02/analisis.py
+++ semana_02/analisis.py
@@ -4,9 +4,17 @@ def ejecutar_analisis():
   st.subheader("Análisis")
   st.write("Analizando el algoritmo")
   data={
-    "Operacion": ["int count = 0"],
-    "Número de operaciones": ["2"],
-    "¿Por qué?": ["Declaración y asignación"]
+    "Operacion": ["int count = 0", "int i = 0", "i < arr.length", "i++", "sum++", "if(arr[i])", "count++", "return count >= 2"],
+    "Número de operaciones": ["2", "2", "n+1", "2 * n = 2n", "2n", "2", "2"],
+    "¿Por qué?": [
+                  "Declaración y asignación", 
+                  "Declaración y asignación", 
+                  "Se realiza una comparación. La acción se repite n veces. Se realiza una vez más como falsa", 
+                  "Acción suma y asignación. Se repite n veces.", 
+                  "Acción suma y asignación. Se repite n veces por el bucle.", 
+                  "Condición y acceso a valor del vector, se repite n veces.", 
+                  "Declaración y asignación", 
+                  "Comparación y devolver valor"]
   }
   df=pd.DataFrame(data)
   st.table(df)
