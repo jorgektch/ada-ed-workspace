d46e1bd85180cf18fef1b065f5c7d80c8883dc4a
Alvaro Chavez <135331266+AlvaroChavezCc@users.noreply.github.com>
2024-05-09 00:04:41 -0500

    Update analisis.py

--- semana_02/analisis.py
+++ semana_02/analisis.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-def ejecutar_analisis():
+def ejecutar_analisis11():
   st.subheader("Análisis")
   st.write("Analizando el algoritmo")
   data={
@@ -23,6 +23,47 @@ def ejecutar_analisis():
       T(n) = 5n + 9
       ''')
   st.markdown("**Suposición:** BigO es O(n)")
+  st.latex(r'''
+      \text{Hallar una constante c y un valor } n_0 \text{ tal que:} \\
+      T(n) \leq cn \text{, para todo } n \geq n_0
+      ''')
+  st.markdown("**Con c=6:**")
+  st.latex(r'''
+      5n + 9 \leq 6n \\
+      9 \leq n
+      ''')
+  st.write("Se obtiene:")
+  st.latex(r'''c = 6 \text{ y } n_0 = 3''')
+  st.markdown("**T(n) = O(n):**")
+  st.latex(r'''
+      \text{Dado un } c = 6 \text{ para todo } n \geq 9: \\
+      5n + 9 \leq 6n \text{, para todo } n \geq 9
+      ''')
+
+def ejecutar_analisis12():
+  st.subheader("Análisis")
+  st.write("Analizando el algoritmo")
+  data={
+    "Operacion": ["int i = 0", "i < arr.length", "i++", "int j = i + 1", "j < arr.length", "j++", "if(arr[i] && arr[j])", "return true", "return false"],
+    "Número de operaciones": ["2", "n+1", "2 * n = 2n", "3 * (n - 1) = 3n - 3", "n * (n - 1) / 2", "2 * n * (n - 1) / 2", "3 * n * (n - 1) / 2", "0", "1"],
+    "¿Por qué?": [
+                  "Declaración y asignación",
+                  "Se realiza una comparación. La acción se repite n veces. Se realiza una vez más como falsa", 
+                  "Acción suma y asignación. Se repite n veces.", 
+                  "Declaración, acción suma y asignación. Se repite n - 1 veces", 
+                  "Se realiza una comparación. Por el bucle externo, se repetirá como una sumatoria de n - 1 a 1", 
+                  "Declaración y asignación. Por el bucle externo, se repetirá como una sumatoria de n - 1 a 1", 
+                  "Condición y acceso a valor de los vectores, por el bucle externo, se repetirá como una sumatoria de n - 1 a 1",
+                  "No se considera trabajo",
+                  "Se considera trabajo"]
+  }
+  df=pd.DataFrame(data)
+  st.table(df)
+  st.markdown("**Peor caso:** No hay valores verdaderos")
+  st.latex(r'''
+      T(n) = 5n + 9
+      ''')
+  st.markdown("**Suposición:**")
   st.latex(r'''
       \text{Hallar una constante c y un valor } n_0 \text{ tal que:} \\
       T(n) \leq cn \text{, para todo } n \geq n_0
