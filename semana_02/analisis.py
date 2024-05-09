bfe642c41c5d70e460f67b133a7e1a6d7014f9b7
Alvaro Chavez <135331266+AlvaroChavezCc@users.noreply.github.com>
2024-05-08 23:02:23 -0500

    Update analisis.py

--- semana_02/analisis.py
+++ semana_02/analisis.py
@@ -1,7 +1,15 @@
 import streamlit as st
-
+import pandas as pd
 def ejecutar_analisis():
   st.subheader("Análisis")
+  st.write("Analizando el algoritmo")
+  data={
+    "Operacion": ["int count = 0"],
+    "Número de operaciones": ["2"],
+    "¿Por qué?": ["Declaración y asignación"]
+  }
+  df=pd.DataFrame(data)
+  st.table(df)
   st.latex(r'''
       a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
       \sum_{k=0}^{n-1} ar^k =
