e6f495cbda91a829fef0131f5bcd4d08204d62b8
Alvaro Chavez <135331266+AlvaroChavezCc@users.noreply.github.com>
2024-05-08 22:41:27 -0500

    Update implementacion.py

--- semana_01/implementacion.py
+++ semana_01/implementacion.py
@@ -3,7 +3,14 @@ import streamlit as st
 def ejecutar_implementacion():
   st.subheader("Implementación")
 
-  code = '''def hello():
-    print("Hello, Streamlit!")'''
-  st.code(code, language='python')
+  code = '''public boolean hasTwoTrueValues( boolean [ ] arr ){
+	int count = 0;
+	
+	for( int i = 0; i < arr.length; i++ )
+		if( arr[ i ] )
+			count++;
+
+	return count >= 2;
+ }'''
+  st.code(code, language='java')
 
