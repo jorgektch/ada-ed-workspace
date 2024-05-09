968e757189a4bd756462c6fb6dc831d3218a4ad4
Alvaro Chavez <135331266+AlvaroChavezCc@users.noreply.github.com>
2024-05-08 22:36:11 -0500

    Update enunciado.py

--- semana_01/enunciado.py
+++ semana_01/enunciado.py
@@ -2,6 +2,4 @@ import streamlit as st
 
 def ejecutar_enunciado():
   st.subheader("Ejercicio 5.24")
-  st.write("El método hasTwoTrueValues devuelve un valor verdadero
-  si al menos dos valores de una matriz booleana son verdederos. Proporcione 
-  el tiempo de ejecución O mayúscula para las tres implementaciones propuestas")
+  st.write("El método hasTwoTrueValues devuelve un valor verdadero si al menos dos valores de una matriz booleana son verdederos. Proporcione el tiempo de ejecución O mayúscula para las tres implementaciones propuestas")
