331dc4bab02e21636f55f8118d1a156c4926ae56
Alvaro Chavez <135331266+AlvaroChavezCc@users.noreply.github.com>
2024-05-09 02:16:06 -0500

    Update analisis.py

--- semana_02/analisis.py
+++ semana_02/analisis.py
@@ -180,4 +180,93 @@ def ejecutar_analisis3():
       \text{ } \\
       \text{Un valor válido para } n_0 \text{ sería: } 17 \\
       ''')
-  
+
+def ejecutar_analisis4():
+  st.subheader("Análisis")
+  st.write("Analizando el algoritmo")
+  st.markdown("**Peor caso:** Que n sea impar")
+  data={
+    "Operacion": ["int i = 0", "i < n", "i++", "int j = 0", "j < n", "j++", "sum++"],
+    "Número de operaciones": ["2", "n + 1", "2n", "2n", "n^2 + 1", "2n^2", "2n^2"],
+    "¿Por qué?": [
+                  "Declaración y asignación",
+                  "Se realiza una comparación. La acción se repite (n - 1 + 0 + 1) veces. Se hace una vez más como falsa.", 
+                  "Acción suma y asignación. Se repite (n - 1 + 0 + 1) veces.", 
+                  "Acción y declaración. Se repite n veces por el bucle externo.",
+                  "Se realiza una comparación. La acción se repite (n - 1 + 0 + 1) veces. Se hace una vez más como falsa. Se repite n veces por el bucle externo.",
+                  "Acción suma y asignación. Se repite (n - 1 + 0 + 1) veces. Se repite n veces por el bucle externo. ",
+                  "Acción suma y asignación. Se repite n veces por el bucle interno. Se repite n veces por el bucle externo."]
+  }
+  df=pd.DataFrame(data)
+  st.table(df)
+  st.markdown("**El T(n) es:**")
+  st.latex(r'''
+      T(n) = 5n^2 + 6n + 3
+      ''')
+  st.markdown("**Suposición:** BigO es O(n^2)")
+  st.markdonw("**Sea:**")
+  st.latex(r'''
+      5n^2 \leq 5n^2 \text{...(1)} \\
+      6n \leq 6n^2 \text{...(2)} \\
+      3 \leq 3n^2 \text{...(3)} \\
+      ''')
+  st.markdown("**Sumando (1) , (2) y (3):**")
+  st.latex(r'''
+      5n^2 + 6n + 3 \leq 14n^2 \\
+      6n + 3 \leq 9n^2 \\
+      0 \leq 9n^2 - 6n - 3 \\
+      0 \leq 3n^2 - 2n - 1 \\
+      ''')
+  st.markdown("**Resolviendo se tiene: **")
+  st.latex(r'''
+      n \leq \frac{-1}{3} \text{ y } n \geq 1 \\
+      c = 14 \\
+      n_0 = 1 \\
+      ''')
+  st.markdown("**T(n) = O(n^2):**")
+  st.latex(r'''
+      \text{Dado un c= 14 para todo } n \geq 1 \\
+      5n^2 + 6n + 3 \leq 14n^2 \text{, para todo } n \geq 1 \\
+      ''')
+def ejecutar_analisis5():
+  st.subheader("Análisis")
+  st.write("Analizando el algoritmo")
+  st.markdown("**Peor caso:** Que n sea impar")
+  data={
+    "Operacion": ["int i = 0", "i < n", "i++", "sum++", "int j = 0", "j < n", "j++", "sum++"],
+    "Número de operaciones": ["2", "n + 1", "2n", "2n", "2", "n + 1", "2n", "2n"],
+    "¿Por qué?": [
+                  "Declaración y asignación",
+                  "Se realiza una comparación. La acción se repite (n - 1 + 0 + 1) veces. Se hace una vez más como falsa.", 
+                  "Acción suma y asignación. Se repite (n - 1 + 0 + 1) veces.", 
+                  "Acción y declaración. Se repite n veces por el bucle externo.",
+                  "Declaración y asignación",
+                  "Se realiza una comparación. La acción se repite (n - 1 + 0 + 1) veces. Se hace una vez más como falsa.", 
+                  "Acción suma y asignación. Se repite (n - 1 + 0 + 1) veces.", 
+                  "Acción y declaración. Se repite n veces por el bucle externo."]
+  }
+  df=pd.DataFrame(data)
+  st.table(df)
+  st.markdown("**El T(n) es:**")
+  st.latex(r'''
+      T(n) = 10n + 6
+      ''')
+  st.markdown("**Suposición:** BigO es O(n)")
+  st.latex(r'''
+      \text{Hallar una cosntante c u valor } n_0 \text{tal que: }\\
+      T(n) \leq cn \text{, para todo } n \geq n_0 \\
+      ''')
+  st.markdown("**Con c = 11:**")
+  st.latex(r'''
+      10n + 6 \leq 11n \\
+      6 \leq n \\
+      ''')
+  st.markdown("**Se obtiene:**")
+  st.latex(r'''
+      c = 11 \text{ y } n_0 = 6
+      ''')
+  st.markdown("**T(n) = O(n):**")
+  st.latex(r'''
+      \text{Dado un c= 11 para todo } n \geq 6 \\
+      10n + 6 \leq 11n \text{, para todo } n \geq 6 \\
+      ''')
