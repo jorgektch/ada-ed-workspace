e02d01561839a674eef0dc1d20e5f3791985fc06
Alvaro Chavez <135331266+AlvaroChavezCc@users.noreply.github.com>
2024-05-09 01:16:41 -0500

    Update enunciado.py

--- semana_02/enunciado.py
+++ semana_02/enunciado.py
@@ -3,3 +3,11 @@ import streamlit as st
 def ejecutar_enunciado():
   st.subheader("Ejercicio 5.24")
   st.write("El método hasTwoTrueValues devuelve un valor verdadero si al menos dos valores de una matriz booleana son verdederos. Proporcione el tiempo de ejecución O mayúscula para las tres implementaciones propuestas")
+
+def ejecutar_enunciado2():
+  st.subheader("Ejercicio 5.26.1")
+  st.write("Calcular el BigO del siguiente algoritmo.")
+
+def ejecutar_enunciado3():
+  st.subheader("Ejercicio 5.26.2")
+  st.write("Calcular el BigO del siguiente algoritmo.")
