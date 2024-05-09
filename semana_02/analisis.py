67120e75ad2f8c62c077e38a318cf26291b0834d
Alvaro Chavez <135331266+AlvaroChavezCc@users.noreply.github.com>
2024-05-08 23:19:54 -0500

    Update analisis.py

--- semana_02/analisis.py
+++ semana_02/analisis.py
@@ -17,7 +17,7 @@ def ejecutar_analisis():
                   "Comparación y devolver valor"]
   }
   df=pd.DataFrame(data)
-  st.table(df, index=False)
+  st.table(df)
   st.latex(r'''
       a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
       \sum_{k=0}^{n-1} ar^k =
