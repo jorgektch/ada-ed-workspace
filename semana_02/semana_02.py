842a27a4e423cfc676038d88c11022a2457c3b34
Alvaro Chavez <135331266+AlvaroChavezCc@users.noreply.github.com>
2024-05-08 23:52:12 -0500

    Update semana_02.py

--- semana_02/semana_02.py
+++ semana_02/semana_02.py
@@ -7,6 +7,8 @@ from .graficas import *
 def ejecutar_semana_02():
   st.header(f"Semana 02")
   ejecutar_enunciado()
-  ejecutar_implementacion()
+  ejecutar_implementacion10()
   ejecutar_analisis()
+  ejecutar_implementacion11()
+  ejecutar_implementacion12()
   ejecutar_graficas()
