ed1d1acbf446880f4ac272a8e7709a7aaeaf55cb
cat_ta4 <hugoalva29@hotmail.com>
2024-06-23 18:43:00 -0500

    test remote compoent host

--- stretch_component/__init__.py
+++ stretch_component/__init__.py
@@ -76,15 +76,10 @@ def my_component(name, key=None):
     # There's no need to do this in our simple example - but it's an option.
     return component_value
 
-if not _RELEASE:
-    _component_func = components.declare_component(
-        "stretch_component",
-        url="http://localhost:3001",
-    )
-else:
-    parent_dir = os.path.dirname(os.path.abspath(__file__))
-    build_dir = os.path.join(parent_dir, "frontend/build")
-    _component_func = components.declare_component("stretch_component", path=build_dir)
+
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+build_dir = os.path.join(parent_dir, "frontend/build")
+component_func = components.declare_component("stretch_component", path=build_dir)
 
 
 def stretch_component(radius:int, speed:int, numberOfNodes: int, theme:str = "dark", key=None):
