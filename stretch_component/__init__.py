a553fab42b40138dd2684e940d2581c94f07865f
cat_ta4 <hugoalva29@hotmail.com>
2024-06-23 19:13:35 -0500

    add build frontend

--- stretch_component/__init__.py
+++ stretch_component/__init__.py
@@ -2,12 +2,9 @@ import os
 import streamlit.components.v1 as components
 
 
-absolute_path  = os.path.dirname(os.path.abspath(__file__))
-frontend_path = absolute_path
-component_func = components.declare_component(
-    "stretch_component",
-    path=frontend_path
-)
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+build_dir = os.path.join(parent_dir, "frontend/build")
+component = components.declare_component("stretch_component", path=build_dir)
 
 
 def stretch_component(radius:int, speed:int, numberOfNodes: int, theme:str = "dark", key=None):
