d4d707ffb1ef221497b8b7e69574640d5b373bff
cat_ta4 <hugoalva29@hotmail.com>
2024-06-23 19:16:41 -0500

    correct wrong declaration variable

--- stretch_component/__init__.py
+++ stretch_component/__init__.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 
 parent_dir = os.path.dirname(os.path.abspath(__file__))
 build_dir = os.path.join(parent_dir, "frontend/build")
-component = components.declare_component("stretch_component", path=build_dir)
+component_func = components.declare_component("stretch_component", path=build_dir)
 
 
 def stretch_component(radius:int, speed:int, numberOfNodes: int, theme:str = "dark", key=None):
