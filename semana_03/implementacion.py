8ac47910c6c44714547415577336606a5f59e0c8
frank777ch <150204523+frank777ch@users.noreply.github.com>
2024-06-26 20:37:19 -0500

    Avance xd
    Este es mi avance

--- semana_03/implementacion.py
+++ semana_03/implementacion.py
@@ -1,9 +1,265 @@
 import streamlit as st
 
-def ejecutar_implementacion():
+def ejecutar_implementacion1():
   st.subheader("Implementación")
 
-  code = '''def hello():
-    print("Hello, Streamlit!")'''
+  code = '''def div_Entera(numerador, denominador):
+    if numerador < denominador:
+        return 0
+    else:
+        return 1 + div_Entera(numerador - denominador, denominador)
+        
+    numerador = int(input("Ingrese el numerador: "))
+    denominador = int(input("Ingrese el denominador: "))
+
+    resultado = div_Entera(numerador, denominador)
+
+    print("La división entera es:", resultado)
+    '''
   st.code(code, language='python')
 
+def ejecutar_implementacion2():
+  st.subheader("Implementación")
+
+  code = '''def invertir_Numero(num, pos):
+    if num < 10:
+        return num
+    else:
+        ultimo_digito = num % 10
+        return ultimo_digito * (10*pos) + invertir_Numero(num / 10, pos - 1)
+
+    numero = int(input("Ingrese un número a invertir: "))
+    num_Digitos = len(str(numero)) - 1
+
+    resultado = invertir_Numero(numero, num_Digitos)
+    print("El número invertido es:", resultado)
+    '''
+  st.code(code, language='python')
+
+def ejecutar_implementacion3():
+  st.subheader("Implementación")
+
+  code = '''public class Main {
+    
+    public static int sumarDigitos(int numero) {
+        if(numero < 10) {
+            return numero;
+        } else {
+            return (numero % 10) + sumarDigitos(numero / 10);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresar numero: ");
+        int num = scanner.nextInt();
+        int suma = sumarDigitos(num);
+        System.out.println("Suma: " + suma);
+        scanner.close();
+    }
+  }
+  '''
+  st.code(code, language='java')
+
+def ejecutar_implementacion4():
+  st.subheader("Implementación")
+
+  code = '''public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese num1: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Ingrese num2: ");
+        int num2 = scanner.nextInt();
+        int resultado = multiplicar(num1, num2);
+        System.out.println("Resultado: " + resultado);
+    }
+
+    public static int multiplicar(int a, int b) {
+        if (a == 0 || b == 0) {
+            return 0;
+        } else if (b == 1) {
+            return a;
+        } else if (a == 1) {
+            return b;
+        } else {
+            return a + multiplicar(a, b - 1);
+        }
+      }
+    }
+    '''
+  st.code(code, language='java')
+
+def ejecutar_implementacion5():
+  st.subheader("Implementación")
+
+  code = '''public class Problema5 {
+    
+    public static void main(String[] args) {
+        int[] vector = {1, 2, 3, 4, 7};
+        int resultado = sumaVector(vector, 5);
+        System.out.println("La suma de los elementos del vector es: " + resultado);
+    }
+    
+    public static int sumaVector(int[] vector, int indice) {     
+
+   //Funcion recursiva
+        if (indice == 1) {
+            return vector[indice-1]; //Caso base, operaciones primitivas c1
+        } else {
+            return vector[indice-1] + sumaVector(vector, indice - 1);
+        }  
+      }
+    }
+    '''
+  st.code(code, language='java')
+
+def ejecutar_implementacion6():
+  st.subheader("Implementación")
+
+  code = '''package problema6;
+import java.util.Scanner;
+
+public class Problema6 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa un numero: ");
+        double numero = scanner.nextDouble();
+
+        if (esPositivo(numero)) {
+            System.out.println(numero + " es positivo.");
+        } else if (esNegativo(numero)) {
+            System.out.println(numero + " es negativo.");
+        } else {
+            System.out.println(numero + " es cero.");
+        }
+    }
+
+    public static boolean esPositivo(double numero) {
+        if (numero > 0) {
+            return true;
+        } else if (numero == 0) {
+            return false;
+        } else {
+            return esPositivo(numero + 1);
+        }
+    }
+
+    public static boolean esNegativo(double numero) {
+        if (numero < 0) {
+            return true;
+        } else if (numero == 0) {
+            return false;
+        } else {
+            return esNegativo(numero - 1);
+        }
+      }
+    }
+    '''
+  st.code(code, language='java')
+
+def ejecutar_implementacion7():
+  st.subheader("Implementación")
+
+  code = ''' static int digitSum (int n) {
+    if (n < 10) { 
+                  return n;
+    } else {
+                return n % 10 + digitSum(n / 10); 
+    }
+  }
+    '''
+  st.code(code, language='python')
+
+def ejecutar_implementacion8():
+  st.subheader("Implementación")
+
+  code = '''#include <bits/stdc++.h>
+
+    using namespace std;
+
+    string getBin(int n, string bin){
+        string ans = bin;
+        if(n != 0){
+          if(n & (1 << 0)){
+              ans = "1" + ans;
+          }else{
+              ans = "0" + ans;
+          }
+          return getBin(n>>1,ans);
+        }else{
+          return ans;
+        }
+
+    }
+
+    int main(){
+        int n;
+        cin>>n;
+        cout<<getBin(n,"")<<endl;
+    }
+    '''
+  st.code(code, language='cpp')
+
+def ejecutar_implementacion9():
+  st.subheader("Implementación")
+
+  code = '''def div_Entera(numerador, denominador):
+    if numerador < denominador:
+        return 0
+    else:
+        return 1 + div_Entera(numerador - denominador, denominador)
+        
+    numerador = int(input("Ingrese el numerador: "))
+    denominador = int(input("Ingrese el denominador: "))
+
+    resultado = div_Entera(numerador  , denominador)
+
+    print("La división entera es:", resultado)
+    '''
+  st.code(code, language='python')
+
+def ejecutar_implementacion10():
+  st.subheader("Implementación")
+
+  code = '''#include <bits/stdc++.h>
+    using namespace std;
+
+    double potencia(double n,double x){
+   	 	if(x <= 1){
+   		 	return n;
+   	 	}else{
+   			return n*potencia(n,x-1);
+   	 	}
+    }
+    
+    int main(){
+    		double n,x;
+   	 	cin>>n>>x;
+   	 	cout<<potencia(n,x)<<endl;
+    }
+    '''
+  st.code(code, language='cpp')
+
+def ejecutar_implementacion11():
+  st.subheader("Implementación")
+
+  code = '''#include <bits/stdc++.h>
+    using namespace std;
+
+    int GCD(int a, int b, int factor){
+        if(a % factor == 0 && b % factor == 0){
+          return factor;
+        }else{
+          return max(1,GCD(a,b,factor-1));
+        }
+    }
+
+    int main(){
+        int a,b;
+        cin>>a>>b;
+        cout<<GCD(a,b,a)<<endl;
+    }
+    '''
+  st.code(code, language='cpp')
--- semana_03/semana_03.py
+++ semana_03/semana_03.py
@@ -6,7 +6,58 @@ from .graficas import *
 
 def ejecutar_semana_03():
   st.header(f"Semana 03")
-  ejecutar_enunciado()
-  ejecutar_implementacion()
-  ejecutar_analisis()
-  ejecutar_graficas()
+
+  # Ejercicio 1
+  ejecutar_enunciado1()
+  ejecutar_implementacion1()
+  ejecutar_analisis1()
+
+  # Ejercicio 2
+  ejecutar_enunciado2()
+  ejecutar_implementacion2()
+  ejecutar_analisis2()
+
+  # Ejercicio 3
+  ejecutar_enunciado3()
+  ejecutar_implementacion3()
+  ejecutar_analisis3()
+
+  # Ejercicio 4
+  ejecutar_enunciado4()
+  ejecutar_implementacion4()
+  ejecutar_analisis4()
+
+  # Ejercicio 5
+  ejecutar_enunciado5()
+  ejecutar_implementacion5()
+  ejecutar_analisis5()
+
+  # Ejercicio 6
+  ejecutar_enunciado6()
+  ejecutar_implementacion6()
+  ejecutar_analisis6()
+
+  # Ejercicio 7
+  ejecutar_enunciado7()
+  ejecutar_implementacion7()
+  ejecutar_analisis7()
+
+  # Ejercicio 8
+  ejecutar_enunciado8()
+  ejecutar_implementacion8()
+  ejecutar_analisis8()
+
+  # Ejercicio 9
+  ejecutar_enunciado9()
+  ejecutar_implementacion9()
+  ejecutar_analisis9()
+
+   # Ejercicio 10
+  ejecutar_enunciado10()
+  ejecutar_implementacion10()
+  ejecutar_analisis10()
+
+   # Ejercicio 11
+  ejecutar_enunciado11()
+  ejecutar_implementacion11()
+  ejecutar_analisis11()
