c9fa7b0e429d422a42c655842dd0a801376f7ac3
frank777ch <150204523+frank777ch@users.noreply.github.com>
2024-06-27 15:07:50 -0500

    Semana 1

--- semana_01/enunciado.py
+++ semana_01/enunciado.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
-def ejecutar_enunciado():
-  st.subheader("Enunciado")
-  st.write("Este es el enunciado")
+def ejecutar_enunciado1():
+  st.subheader("Enunciado 1")
+  st.write("Escribir un método, llamado sumPair0, que acepta un array de enteros, vector, como parámetro y devuelve el número de pares (i, j), que cumplen vector [i] + vector [j]= 0. Calcular la funcióndel tiempo de ejecución T (n).")
+
+def ejecutar_enunciado2():
+  st.subheader("Enunciado 2")
+  st.write("Escribir un método, llamado sumTriple0, que acepte un array de enteros, vector, como parámetro y devuelva el número de triples (i, j, k), que cumplen vector [i] + vector [j] + vector [k] = 0 tal que i<j<k. Calcular la función del tiempo de ejecución T (n).")
